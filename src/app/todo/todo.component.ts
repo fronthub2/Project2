684f1ce16949b84a027550e77143d02caad022af
Денис Андросов <you@example.com>
2024-12-02 17:15:50 +0300

    не уверен, что правильно. Еху, мой первый коммит!

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,33 +1,3 @@
-// import { CommonModule } from '@angular/common';
-// import { Component, EventEmitter, Input, Output } from '@angular/core';
-// import { IData } from '../interface/todoitem.interface';
-
-// @Component({
-//   selector: 'app-todo',
-//   imports: [CommonModule],
-//   standalone: true,
-//   templateUrl: './todo.component.html',
-//   styleUrl: './todo.component.scss',
-// })
-// export class TodoComponent {
-//   @Input() todoData!: IData[];
-//   @Output() onData = new EventEmitter<IData[]>();
-
-//   onChangeData(event: IData[]) {
-//     this.onData.emit(event);
-//   }
-
-//   onDelete(event: MouseEvent) {
-//     const target = event.target as HTMLElement;
-//     const parent = target.parentElement as HTMLElement;
-//     const id = parent.id;
-//     this.todoData = this.todoData.filter((d) => d.id !== Number(id));
-//     this.todoData.forEach((d, i: number) => (d.id = i + 1));
-
-//     this.onChangeData(this.todoData);
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IData } from '../interface/todoitem.interface';

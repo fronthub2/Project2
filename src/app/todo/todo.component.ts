7dc2f09be753131b37db3b779c6cc4d284be6519
Денис Андросов <you@example.com>
2024-12-05 19:51:27 +0300

    update loading animation

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IData } from '../interface/todoitem.interface';
 
 @Component({
@@ -9,17 +9,26 @@ import { IData } from '../interface/todoitem.interface';
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('до', this.isLoading);
+    const timeout = setTimeout(() => {
+      this.isLoading = false;
+      console.log('до', this.isLoading);
+    }, 2000);
+  }
+
   @Input() valueData!: IData[];
   @Output() onDeleteEvent = new EventEmitter<number>();
-  @Input() isDisableDelete!:boolean;
+  @Input() isDisableDelete!: boolean;
   @Output() onEditEvent = new EventEmitter<number>();
+  isLoading: boolean = true;
 
   onDelete(index: number) {
     this.onDeleteEvent.emit(index);
   }
 
-  onEdit(index:number) {
+  onEdit(index: number) {
     this.onEditEvent.emit(index);
   }
 }
